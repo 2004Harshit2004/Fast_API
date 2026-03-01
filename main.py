93040d3b70d03fc1284f1f9f22996439e7e4d8a0
2004Harshit2004 <harshitgoswami974@gmail.com>
2026-03-01 14:36:40 +0530

    added get method view

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import json
+
+app=FastAPI()
+
+def load_data():
+    with open('patients.json') as f:
+        data = json.load(f)
+        return data
+
+@app.get("/")
+def hello():
+    return {"message":"Patient Management System API"}
+
+@app.get("/about")
+def about():
+    return {"message":"An API system for patient record management"}
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
